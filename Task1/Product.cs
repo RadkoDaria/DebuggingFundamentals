782c0dde6291868a8c4e1dad52eba41d3345f3ad
Darya_Kotova <darya_kotova@epam.com>
2020-12-03 21:47:19 +0600

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product 
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Product))
+            {
+                return false;
+            }
+            return (this.Name == ((Product)obj).Name)
+                && (this.Price == ((Product)obj).Price);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Price.GetHashCode();
+        }
+    }
+}
